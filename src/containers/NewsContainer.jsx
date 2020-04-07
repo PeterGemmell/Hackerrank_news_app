06235da9a62302e4843b0d28ae4ce527e771e0d7
Peter Gemmell <petergemmell65@hotmail.com>
2020-04-07 17:45:58 +0100

    filter added

--- src/containers/NewsContainer.jsx
+++ src/containers/NewsContainer.jsx
@@ -1,20 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ArticleList from '../components/ArticleList';
+import Filter from '../components/Filter';
 
 
 class NewsContainer extends React.Component{
   constructor(props){
     super(props);
+
     this.state={
       articles:[],
       filteredarticles:[]
     };
+
+    // this.filter = this.filter.bind(this);
   }
 
-componentDidMount(){
-  this.loadarticles(this.props.articles[0].url)
+  componentDidMount(){
+    this.loadarticles(this.props.articles[0].url);
+
 
 }
+
+
 
 loadarticles(url){
   fetch(url)
@@ -28,14 +35,24 @@ loadarticles(url){
 
     Promise.all(filteredarticles)
     .then((results) => {
-      this.setState({filteredarticles: results})
-    }) // promise is fetching everything from an array.
-  })
+      this.setState({articles: results, filteredarticles: results})
+    }); // promise is fetching everything from an array.
+  });
 
 }
+  //
+  // filter(searchTerm){
+  //   const lowerSearch = searchTerm.toLowerCase();
+  //   const filteredarticles = this.state.articles.filter((article) => {
+  //     article.title.toLowerCase().indexOf(lowerSearch) > -1;
+  //   });
+  //   this.setState({ filteredarticles: filteredarticles });
+  // }
+
 render(){
   return(
     <div>
+    <Filter handleChange={this.filter}/>
    <ArticleList articles={this.state.filteredarticles}/>
 
     </div>
